ef691ee82cf8bede7f32fe8b0384c2593f06a9f1
felt20 <joshua.ramos18@yahoo.com.ph>
2018-08-01 10:28:05 +0800

    Add all_query

--- server/db/sql query/q_all.sql
+++ server/db/sql query/q_all.sql
@@ -17,36 +17,15 @@ SELECT * FROM sensor_data sa;
     FROM bin b, location l
    WHERE b.location_id = l.location_id;
 
--- All employee information: name and occupation
--- -------------------------------------
-  SELECT employee_id, first_name, last_name
-    FROM employee
-ORDER BY employee.last_name;
-
--- All employee activity information: employee name, cleaned bin, timestamp
--- -------------------------------------
-  SELECT e.employee_id, e.first_name, e.last_name, 
-         b.name AS cleaned_bin, ea.activity_timestamp 
-    FROM bin b, employee e, employee_activity ea
-   WHERE b.bin_id = ea.bin_id 
-     AND ea.employee_id = e.employee_id
-ORDER BY ea.activity_timestamp;
-
--- All sensor data information: bin name, temperature, humidity, lid status, timestamp
+-- All sensor data information: bin name, waste height, temperature, humidity, weight, timestamp
 -- -------------------------------------
   SELECT sd.data_id, b.name AS bin_name, 
          sd.waste_height, sd.temperature, 
-         sd.humidity, sd.is_open AS lid_status, sd.data_timestamp
+         sd.humidity, sd.weight, sd.timestamp
     FROM bin b, sensor_data sd
    WHERE b.bin_id = sd.bin_id
-ORDER BY sd.data_timestamp;
+ORDER BY sd.timestamp;
 
--- get num of bins, employees
--- 
-
-
-SELECT e.employee_id, b.bin_id, ea.activity_timestamp
-    FROM bin b, employee e, employee_activity ea
-   WHERE b.bin_id = ea.bin_id 
-     AND ea.employee_id = e.employee_id
-ORDER BY ea.activity_timestamp;
+-- Get number of bins
+  SELECT COUNT(b.bin_id) AS bin_total
+    FROM bin b;
